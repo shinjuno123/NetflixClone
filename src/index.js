43aa81cc1dd2f91a03b262fd696e18dbe22ec11c
shinjuno <shinjuno123@naver.com>
2021-02-02 18:37:52 +0900

    upload

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import logo from './images/netflix-logo.png';
 import searchIcon from './images/search.png';
@@ -6,6 +6,8 @@ import bellIcon from './images/bell.PNG';
 import giftIcon from './images/gift.PNG';
 import profileIcon from './images/profile.png';
 import caretIcon from './images/caret.png'
+import rightPointer from './images/right-p.png';
+import exclamaiotn_point from './images/exclamation.png';
 import './index.css';
 
 class NavButton extends React.Component{
@@ -83,8 +85,8 @@ class Navi extends React.Component{
 
 function DramaImage(){
     const [dimensions,setDimenstions] = React.useState({
-        height: window.innerHeight,
-        width: window.innerHeight
+        height: window.innerWidth *0.5625,
+        width: window.innerWidth-50
     })
 
     React.useEffect(()=>{
@@ -92,7 +94,7 @@ function DramaImage(){
         function handleResize(){
             setDimenstions({
                 height:window.innerWidth *0.5625,
-                width: window.innerWidth
+                width: window.innerWidth-50
             })
         }
         window.addEventListener('resize',handleResize);
@@ -112,12 +114,103 @@ function DramaImage(){
     
 }
 
+
+function Description(){
+    const [state] = React.useState({
+        description:"암살 목표는 중학교 교사의 탈을 쓴 괴물. 암살자는 교사의\n반 학생 전체. 하지만 학생들은 선생님과 점점 가까워진다.\n마음을 주고받은 상대를 암살하는 일은 가능할까.",
+    });
+
+    const des_array = state.description.split('\n').map((line)=>{
+        return <div>{line}</div>
+    });
+
+
+
+    console.log(state.description);
+    return(
+        <div className="contents-box">
+            <div className="right-side">
+                <img className="drama-title" src="https://occ-0-395-988.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABVn-tQq5sE2XRPhqFqoWiDssdKSp0UUzTC6pDauMHd1sXAf3bL-G3P82-0phmofkqneGGtnxTOJZ5qPBiaWVwuF8gKSo8ifv8WTv.webp?r=150" alt="image"></img>
+                <div className="description-sentence">{des_array}</div>
+                <div className="button-box">
+                    <button className="play-button" type="button" ><img className="right-pointer" alt="image" src={rightPointer}/><h3 className="play-word">재생</h3></button>
+                    <button className="detail-button" type="button" ><img className="exclamation-point" alt="image" src={exclamaiotn_point}/><h3 className="detail-word">상세 정보</h3></button>
+                </div>
+            </div>
+            <div className="left-side">
+                <div></div>
+            </div>
+        </div>
+        
+    );
+}
+
+
+function Drama(){
+    return(
+        <div className="drama">
+            <div className="video"></div>
+            <div className="video-gage"></div>
+        </div>
+    );
+}
+
+
+function DramaLay(){
+
+    const [state,setState] = useState(
+        {
+            length: 9,
+            array: Array(9).push("dsd"),
+        }
+    );
+    
+
+    return(
+        <div className="drama-lay">
+            <div className="empty-lay">
+                <div className="row-header"></div>
+            </div>
+            <div className="drama-row">
+                <Drama/>
+                <Drama/>
+                <Drama/>
+                <Drama/>
+                <Drama/>
+                <Drama/>
+                <Drama/>
+                <Drama/>
+                <Drama/>
+                <Drama/>
+                <Drama/>
+            </div>
+        </div>
+    );
+}
+
+
 class Main extends React.Component{
     render(){
         return(
             <div className="total-screen">
-                <Navi/>
+                <header>
+                    <Navi/>
+                </header>
                 <DramaImage/>
+                <div className="contents">
+                    <Description/>   
+                    <div className="age-box">
+                        <div className="empty"></div>
+                        <div className="age-limit"><div className="label"><div className="age">15+</div></div></div>
+                    </div> 
+                    
+                </div>
+                <DramaLay/>
+                <footer>
+
+                </footer>
+
+                
             </div>
         );
     }
